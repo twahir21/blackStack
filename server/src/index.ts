4b47b0ab20c210cb173c4677c3302b6976314293
twahir21 <twahirsudy3@gmail.com>
2025-08-26 17:23:59 +0300

    changing welcoming message for elysia api

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get("/", () => "Hello, from blackStack Elysia API").listen(3000);
 
 console.log(`
   🚀 BlackStack Server initialized: 🦊 Elysia is running ... at http://${app.server?.hostname}:${app.server?.port}
